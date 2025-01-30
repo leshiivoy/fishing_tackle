d2f7a2cb5fa7a9e5c11c76048a14c54a6c9ca316
leshiivoy <leshiivoy@gmail.com>
2025-01-30 23:22:32 +0200

    Статические файлы. Добавление Bootstrap 1

--- fishing_tackle/urls.py
+++ fishing_tackle/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
