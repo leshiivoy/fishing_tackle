c067ec5e3d52e1a8ccd841c221e5f56503da1960
leshiivoy <leshiivoy@gmail.com>
2025-01-30 16:32:45 +0200

    Начало работы с шаблонами html

--- main/views.py
+++ main/views.py
@@ -4,7 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html')
+    context = {
+        'title': 'Home',
+        'content': 'Самый лучший интернет магазин рыболовных снастей - Home',
+        'list': ['first', 'second'],
+        'dict': {'first': 1},
+        'bool': True
+    }
+
+    return render(request, 'main/index.html', context)
 
 
 def about(request):
