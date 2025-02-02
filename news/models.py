25609f7dea08c05f78be4dbffd32f105f20fd751
leshiivoy <leshiivoy@gmail.com>
2025-02-02 17:45:11 +0200

    Форма для добавления записей

--- news/models.py
+++ news/models.py
@@ -5,7 +5,7 @@ class Articles(models.Model):
     title = models.CharField('Название', max_length=50)
     anons = models.CharField('Анонс', max_length=250)
     full_text = models.TextField('Статья')
-    data = models.DateTimeField('Дата публикации')
+    date = models.DateTimeField('Дата публикации')
 
     def __str__(self):
         return self.title
